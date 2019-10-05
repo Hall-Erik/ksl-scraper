212a4e62b80dd8a23eccf3db8a9a4503b597c3dd
Erik <hall.erik@gmail.com>
2019-10-05 15:36:45 -0600

    Added models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Job(models.Model):
+    name = models.CharField(max_length=120)
+    employer = models.CharField(max_length=64)
+    url = models.CharField(max_length=300, unique=True, db_index=True)
+    date_posted = models.CharField(max_length=10)
+
+
+class SeenJobs(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class HiddenJobs(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
