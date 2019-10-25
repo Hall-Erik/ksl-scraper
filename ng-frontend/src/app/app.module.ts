5df8f4242923572fd2e94ac2a4e5538bcaec9f90
Erik <hall.erik@gmail.com>
2019-10-25 14:05:31 -0600

    added flex for more styling

--- ng-frontend/src/app/app.module.ts
+++ ng-frontend/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialModule } from './material/material.module';
 
@@ -32,6 +32,7 @@ import { JobCardComponent } from './jobs/job-card/job-card.component';
     BrowserAnimationsModule,
     MaterialModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
     HttpClientModule,
     HttpClientXsrfModule.withOptions({
       cookieName: 'csrftoken',
