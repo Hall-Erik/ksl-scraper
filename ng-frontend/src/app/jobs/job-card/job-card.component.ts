c11ec698e006c40bd9c0c3c2ddb5779b75ebe9bc
Erik <hall.erik@gmail.com>
2019-10-28 12:12:21 -0600

    no only logged-in user can see Hide button and New tag

--- ng-frontend/src/app/jobs/job-card/job-card.component.ts
+++ ng-frontend/src/app/jobs/job-card/job-card.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
+import { UserService } from '../../services/user.service';
 import { JobService } from '../../services/job.service';
+import { User } from '../../models/user';
 import { Job } from '../../models/job';
 
 @Component({
@@ -8,11 +10,18 @@ import { Job } from '../../models/job';
   templateUrl: './job-card.component.html',
   styleUrls: ['./job-card.component.css']
 })
-export class JobCardComponent {
+export class JobCardComponent implements OnInit {
   @Input() job: Job;
   @Output() hideJob: EventEmitter<number> = new EventEmitter();
+  user: User;
 
-  constructor(private jobService: JobService) { }
+  constructor(private jobService: JobService,
+              private userService: UserService) { }
+
+  ngOnInit() {
+    this.user = this.userService.user.getValue();
+    this.userService.user.subscribe(user => this.user = user);
+  }
 
   onHide() { this.hideJob.emit(this.job.id); }
 
